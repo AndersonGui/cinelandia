0f69a51b7863dc5b8f79846054be9ae0b299a8e2
Ânderson Guimarães Soares <andersogui@gmail.com>
2019-11-06 15:46:19 -0300

    Correções básicas sem mexer na estrutura

--- cinelandiaViewModel.js
+++ cinelandiaViewModel.js
@@ -1,11 +1,11 @@
 function CinelandiaViewModel() {
     var self = this;
-    
+
     self.Filmes = "";
 
-    self.Filme = ko.observable(new function(){
+    self.Filme = ko.observable(new function () {
         var model = this;
-        
+
         model.Nome = ko.observable("It: A Coisa");
         model.Ano = ko.observable(2017);
         model.Categoria = ko.observable("Terror");
@@ -13,49 +13,44 @@ function CinelandiaViewModel() {
         model.Sinopse = ko.observable("Quando as crianças começam a desaparecer na cidade de Derry, no Maine, as crianças do bairro se unem para atacar Pennywise, um palhaço malvado, cuja história de assassinato e violência remonta há séculos.");
         model.Atores = ko.observableArray(["Bil Skarsgard", "Finn Wolfhard", "Sophia lillis"]);
         model.Img = ko.observable(null);
-        model.Cartaz = ko.computed(x => {
-                return model.Img() != null ? "/imgs/cartaz/"+model.Img()+".jpg" : "";
+        model.Cartaz = ko.computed(x => {            
+            return model.Img() != null ? `./imgs/cartaz/${model.Img()}.jpg` : "";
         }, this);
-    });
-    
-    
-    $.getJSON("filmes.js", function(json) {
-        self.Filmes = json;
-        self.MontarGrid();
-    });
-    
-    self.MontarGrid = function ()  {
+    });  
+
+    self.MontarGrid = function () {
         $("#jsGrid").jsGrid({
             width: "100%",
 
-     
+
             inserting: false,
             editing: false,
             sorting: true,
             paging: false,
-     
+
             data: self.Filmes,
-     
+
             fields: [
-                { name: "Nome", align:"center", type: "text", width: 50},
-                { name: "Ano", align:"center", type: "text", width: 50 },
-                { name: "Categoria", align:"center", type: "text", width: 50 },
-                { name: "Diretor", align:"center", type: "text", width: 50 },
-                { type: "control", width: 30,
-                    itemTemplate: function(value, item) {                   
+                { name: "Nome", align: "center", type: "text", width: 50 },
+                { name: "Ano", align: "center", type: "text", width: 50 },
+                { name: "Categoria", align: "center", type: "text", width: 50 },
+                { name: "Diretor", align: "center", type: "text", width: 50 },
+                {
+                    type: "control", width: 30,
+                    itemTemplate: function (value, item) {
                         var $customButton = $("<i class='fa fa-info-circle fa-2x' aria-hidden='true'></i>")
-                            .click(function(e) {
+                            .click(function (e) {
                                 self.VisualizarModal(item);
                                 e.stopPropagation();
-                            });                        
+                            });
                         return $customButton;
                     }
                 }
             ]
         });
     }
-    
-    self.VisualizarModal = function(filme) {
+
+    self.VisualizarModal = function (filme) {
         self.Filme().Nome(filme.Nome);
         self.Filme().Ano(filme.Ano);
         self.Filme().Categoria(filme.Categoria);
@@ -63,10 +58,12 @@ function CinelandiaViewModel() {
         self.Filme().Sinopse(filme.Sinopse);
         self.Filme().Atores(filme.Atores);
         self.Filme().Img(filme.Cartaz);
-        
-        $('#ModalFilme').modal('show'); 
+
+        $('#ModalFilme').modal('show');
     }
-    
+
+    self.Filmes = filmes;
+    self.MontarGrid();
 }
 
 ko.applyBindings(new CinelandiaViewModel());
--- filmes.js
+++ filmes.js
@@ -1,84 +1,81 @@
-[
-{
-    "IdFilme": 0,
-    "Nome": "It: A Coisa",
-    "Ano": 2017,
-    "Categoria": "Terror",
-    "Diretor":"Andy Muschietti",
-    "Sinopse":"Quando as crianças começam a desaparecer na cidade de Derry, no Maine, as crianças do bairro se unem para atacar Pennywise, um palhaço malvado, cuja história de assassinato e violência remonta há séculos.",
-    "Atores": ["Bil Skarsgard", "Finn Wolfhard", "Sophia lillis"],
-    "Cartaz": "it"
-},
-{
-    "IdFilme": 1,
-    "Nome": "Mother!",
-    "Ano": 2017,
-    "Categoria": "Terror",
-    "Diretor":"Darren Aronofsky",
-    "Sinopse":"A relação de um casal é colocada à prova quando recebem hóspedes que não convidaram e que interrompem as suas tranquilas existências.",
-    "Atores": ["Jennifer Lawrence", "Javier Bardem", "Michelle Pfeiffer"],
-    "Cartaz": "mother"
-},
-{
-    "IdFilme": 2,
-    "Nome": "Closer",
-    "Ano": 2004,
-    "Categoria": "Drama",
-    "Diretor":"Mike Nichols",
-    "Sinopse":"Anna (Julia Roberts) é uma fotógrafa bem sucedida, que se divorciou recentemente. Ela conhece e seduz Dan (Jude Law), um aspirante a romancista que ganha a vida escrevendo obituários, mas se casa com Larry (Clive Owen). Dan mantém um caso secreto com Anna mesmo após ela se casar e usa Alice (Natalie Portman), uma stripper, como musa inspiradora para ganhar confiança e tentar conquistar o amor de Anna.",
-    "Atores": ["Natalie Portman", "Julia Roberts", "Jude Law"],
-    "Cartaz": "closer"
-},
-{
-    "IdFilme": 3,
-    "Nome": "Harry Potter e a Pedra Filosofal",
-    "Ano": 2001,
-    "Categoria": "Fantasia",
-    "Diretor":"Chris Columbus",
-    "Sinopse":"Adaptação da primeira obra popular de J.K. Rowling sobre Harry Potter, um menino que soube em seu aniversário de onze anos que é filho órfão de dois bruxos e possui poderes mágicos únicos. De filho indesejado, passa a ser um estudante de Hogwarts, uma escola inglesa para bruxos. Lá ele conhece vários amigos que o ajudam a descobrir a verdade sobre as mortes misteriosas de seus pais.",
-    "Atores": ["Daniel Radcliffe", "Rupert Grint", "Emma Watson"],
-    "Cartaz": "harry"
-},
-{
-    "IdFilme": 4,
-    "Nome": "Velozes e Furiosos",
-    "Ano": 2001,
-    "Categoria": "Ação",
-    "Diretor":"Rob Cohen",
-    "Sinopse":"Dominic Toretto (Vin Diesel) é o líder de uma gangue de corridas de ruas em Los Angeles que está sendo investigado pela polícia por roubo de equipamentos eletrônicos. Para investigá-lo é enviado Brian O'Conner (Paul Walker), que se infiltra na gangue na intenção de descobrir se Toretto é realmente o autor dos crimes ou se há alguém mais por trás deles.",
-    "Atores": ["Paul Walker", "Vin Diesel", "Michelle Rodriguez"],
-    "Cartaz": "velozes"
-},
-{
-    "IdFilme": 5,
-    "Nome": "Pulp Fiction",
-    "Ano": 1995,
-    "Categoria": "Policial",
-    "Diretor":"Quentin Tarantino",
-    "Sinopse":"Vincent Vega (John Travolta) e Jules Winnfield (Samuel L. Jackson) são dois assassinos profissionais trabalham fazendo cobranças para Marsellus Wallace (Ving Rhames), um poderosos gângster. Vega é forçado a sair com a garota do chefe, temendo passar dos limites; enquanto isso, o pugilista Butch Coolidge (Bruce Willis) se mete em apuros por ganhar luta que deveria perder. ",
-    "Atores": ["John Travolta", "Samuel L. Jackson","Uma Thurman"],
-    "Cartaz": "pulp"
-},
-{
-    "IdFilme": 6,
-    "Nome": "Bingo - O Rei das Manhãs",
-    "Ano": 2017,
-    "Categoria": "Biografia",
-    "Diretor":"Daniel Rezende",
-    "Sinopse":"Cinebiografia de Arlindo Barreto, um dos intérpretes do palhaço Bozo no programa matinal homônimo exibido pelo SBT durante a década de 1980. Barreto alcançou a fama graças ao personagem, apesar de jamais ser reconhecido pelas pessoas por sempre estar fantasiado. Esta frustração o levou a se envolver com drogas, chegando a utilizar cocaína e crack nos bastidores do programa.",
-    "Atores": ["Vladimir Brichta","Leandra Leal","Augusto Madeira"],
-    "Cartaz": "bingo"
-},
-{
-    "IdFilme": 7,
-    "Nome": "Os Vingadores",
-    "Ano": 2012,
-    "Categoria": "Aventura",
-    "Diretor":"Joss Whedon",
-    "Sinopse":"Loki (Tom Hiddleston) retorna à Terra enviado pelos chitauri, uma raça alienígena que pretende dominar os humanos. Com a promessa de que será o soberano do planeta, ele rouba o cubo cósmico dentro de instalações da S.H.I.E.L.D. e, com isso, adquire grandes poderes. Loki os usa para controlar o dr. Erik Selvig (Stellan Skarsgard) e Clint Barton/Gavião Arqueiro (Jeremy Renner), que passam a trabalhar para ele. No intuito de contê-los, Nick Fury (Samuel L. Jackson) convoca um grupo de pessoas com grandes habilidades, mas que jamais haviam trabalhado juntas: Tony Stark/Homem de Ferro (Robert Downey Jr.), Steve Rogers/Capitão América (Chris Evans), Thor (Chris Hemsworth), Bruce Banner/Hulk (Mark Ruffalo) e Natasha Romanoff/Viúva Negra (Scarlett Johansson). Só que, apesar do grande perigo que a Terra corre, não é tão simples assim conter o ego e os interesses de cada um deles para que possam agir em grupo ",
-    "Atores": ["Robert Downey Jr.","Chris Evans,","Mark Ruffalo"],
-    "Cartaz": "vingadores"
-}
-
-
-]
+const filmes = [
+    {
+        "IdFilme": 0,
+        "Nome": "It: A Coisa",
+        "Ano": 2017,
+        "Categoria": "Terror",
+        "Diretor":"Andy Muschietti",
+        "Sinopse":"Quando as crianças começam a desaparecer na cidade de Derry, no Maine, as crianças do bairro se unem para atacar Pennywise, um palhaço malvado, cuja história de assassinato e violência remonta há séculos.",
+        "Atores": ["Bil Skarsgard", "Finn Wolfhard", "Sophia lillis"],
+        "Cartaz": "it"
+    },
+    {
+        "IdFilme": 1,
+        "Nome": "Mother!",
+        "Ano": 2017,
+        "Categoria": "Terror",
+        "Diretor":"Darren Aronofsky",
+        "Sinopse":"A relação de um casal é colocada à prova quando recebem hóspedes que não convidaram e que interrompem as suas tranquilas existências.",
+        "Atores": ["Jennifer Lawrence", "Javier Bardem", "Michelle Pfeiffer"],
+        "Cartaz": "mother"
+    },
+    {
+        "IdFilme": 2,
+        "Nome": "Closer",
+        "Ano": 2004,
+        "Categoria": "Drama",
+        "Diretor":"Mike Nichols",
+        "Sinopse":"Anna (Julia Roberts) é uma fotógrafa bem sucedida, que se divorciou recentemente. Ela conhece e seduz Dan (Jude Law), um aspirante a romancista que ganha a vida escrevendo obituários, mas se casa com Larry (Clive Owen). Dan mantém um caso secreto com Anna mesmo após ela se casar e usa Alice (Natalie Portman), uma stripper, como musa inspiradora para ganhar confiança e tentar conquistar o amor de Anna.",
+        "Atores": ["Natalie Portman", "Julia Roberts", "Jude Law"],
+        "Cartaz": "closer"
+    },
+    {
+        "IdFilme": 3,
+        "Nome": "Harry Potter e a Pedra Filosofal",
+        "Ano": 2001,
+        "Categoria": "Fantasia",
+        "Diretor":"Chris Columbus",
+        "Sinopse":"Adaptação da primeira obra popular de J.K. Rowling sobre Harry Potter, um menino que soube em seu aniversário de onze anos que é filho órfão de dois bruxos e possui poderes mágicos únicos. De filho indesejado, passa a ser um estudante de Hogwarts, uma escola inglesa para bruxos. Lá ele conhece vários amigos que o ajudam a descobrir a verdade sobre as mortes misteriosas de seus pais.",
+        "Atores": ["Daniel Radcliffe", "Rupert Grint", "Emma Watson"],
+        "Cartaz": "harry"
+    },
+    {
+        "IdFilme": 4,
+        "Nome": "Velozes e Furiosos",
+        "Ano": 2001,
+        "Categoria": "Ação",
+        "Diretor":"Rob Cohen",
+        "Sinopse":"Dominic Toretto (Vin Diesel) é o líder de uma gangue de corridas de ruas em Los Angeles que está sendo investigado pela polícia por roubo de equipamentos eletrônicos. Para investigá-lo é enviado Brian O'Conner (Paul Walker), que se infiltra na gangue na intenção de descobrir se Toretto é realmente o autor dos crimes ou se há alguém mais por trás deles.",
+        "Atores": ["Paul Walker", "Vin Diesel", "Michelle Rodriguez"],
+        "Cartaz": "velozes"
+    },
+    {
+        "IdFilme": 5,
+        "Nome": "Pulp Fiction",
+        "Ano": 1995,
+        "Categoria": "Policial",
+        "Diretor":"Quentin Tarantino",
+        "Sinopse":"Vincent Vega (John Travolta) e Jules Winnfield (Samuel L. Jackson) são dois assassinos profissionais trabalham fazendo cobranças para Marsellus Wallace (Ving Rhames), um poderosos gângster. Vega é forçado a sair com a garota do chefe, temendo passar dos limites; enquanto isso, o pugilista Butch Coolidge (Bruce Willis) se mete em apuros por ganhar luta que deveria perder. ",
+        "Atores": ["John Travolta", "Samuel L. Jackson","Uma Thurman"],
+        "Cartaz": "pulp"
+    },
+    {
+        "IdFilme": 6,
+        "Nome": "Bingo - O Rei das Manhãs",
+        "Ano": 2017,
+        "Categoria": "Biografia",
+        "Diretor":"Daniel Rezende",
+        "Sinopse":"Cinebiografia de Arlindo Barreto, um dos intérpretes do palhaço Bozo no programa matinal homônimo exibido pelo SBT durante a década de 1980. Barreto alcançou a fama graças ao personagem, apesar de jamais ser reconhecido pelas pessoas por sempre estar fantasiado. Esta frustração o levou a se envolver com drogas, chegando a utilizar cocaína e crack nos bastidores do programa.",
+        "Atores": ["Vladimir Brichta","Leandra Leal","Augusto Madeira"],
+        "Cartaz": "bingo"
+    },
+    {
+        "IdFilme": 7,
+        "Nome": "Os Vingadores",
+        "Ano": 2012,
+        "Categoria": "Aventura",
+        "Diretor":"Joss Whedon",
+        "Sinopse":"Loki (Tom Hiddleston) retorna à Terra enviado pelos chitauri, uma raça alienígena que pretende dominar os humanos. Com a promessa de que será o soberano do planeta, ele rouba o cubo cósmico dentro de instalações da S.H.I.E.L.D. e, com isso, adquire grandes poderes. Loki os usa para controlar o dr. Erik Selvig (Stellan Skarsgard) e Clint Barton/Gavião Arqueiro (Jeremy Renner), que passam a trabalhar para ele. No intuito de contê-los, Nick Fury (Samuel L. Jackson) convoca um grupo de pessoas com grandes habilidades, mas que jamais haviam trabalhado juntas: Tony Stark/Homem de Ferro (Robert Downey Jr.), Steve Rogers/Capitão América (Chris Evans), Thor (Chris Hemsworth), Bruce Banner/Hulk (Mark Ruffalo) e Natasha Romanoff/Viúva Negra (Scarlett Johansson). Só que, apesar do grande perigo que a Terra corre, não é tão simples assim conter o ego e os interesses de cada um deles para que possam agir em grupo ",
+        "Atores": ["Robert Downey Jr.","Chris Evans,","Mark Ruffalo"],
+        "Cartaz": "vingadores"
+    }]
